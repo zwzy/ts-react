e3b1e1aaf800fc6921cd23c87e698cdfc4eb02ea
zuowang <18755489161@163.com>
2021-07-23 16:19:26 +0800

    feat: 项目初始化

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    HOME
   </React.StrictMode>,
   document.getElementById('root')
 );
