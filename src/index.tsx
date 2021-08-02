0b872f3ddef10f2a8d2459cd7e8b74d482668781
zuowang <18755489161@163.com>
2021-08-02 21:04:58 +0800

    ➕add: 添加react-router-dom路由依赖

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
 ReactDOM.render(
-  <React.StrictMode>
-    HOME
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      HOME
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
