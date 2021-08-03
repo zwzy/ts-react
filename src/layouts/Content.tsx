dc5cb1150438c286de2512db4a067498c0b0ad72
zuowang <18755489161@163.com>
2021-08-03 18:10:54 +0800

    feat: 增加基本页面

---
+++ src/layouts/Content.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const Content = (props: Props) => {
+  return (
+    <div>
+      Content
+    </div>
+  )
+}
+
+export default Content
---
+++ src/pages/Home/Index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const Index = (props: Props) => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Index
