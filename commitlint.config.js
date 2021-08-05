aa35fbfef54170a2c1512733077285bc26a14ce1
zuowang <18755489161@163.com>
2021-08-05 17:41:15 +0800

    chore: 更新配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,31 @@
+const defaultTypes = [
+  'build', // 主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交
+  'ci', // 主要目的是修改项目继续集成流程(例如 Travis，Jenkins，GitLab CI，Circle等)的提交
+  'docs', // 文档更新
+  'feat', // 新增功能
+  'fix', // bug 修复
+  'style', // 不影响程序逻辑的代码修改(修改空白字符，补全缺失的分号等)
+  'refactor', // 重构代码(既没有新增功能，也没有修复 bug)
+  'perf', // 性能优化
+  'test', // 新增测试用例或是更新现有测试
+  'revert', // 回滚某个更早之前的提交
+  'chore', // 不属于以上类型的其他类型(日常事务)
+];
+
+const customTypes = [
+  'util', // 新增项目工具
+  'init', // 项目初始化
+  'add', // 增加依赖
+  'ui', // UI调整
+  'del', // 删除
+];
+
 module.exports = {
   extends: [
     '@commitlint/config-conventional',
-    'cz',
   ],
   rules: {
+    'type-enum': [2, 'always', [...defaultTypes, ...customTypes]],
     'body-leading-blank': [1, 'always'],
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
@@ -17,6 +39,107 @@ module.exports = {
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [0],
     'type-empty': [0],
-    'type-enum': [0],
+  },
+  prompt: {
+    messages: {
+      skip: ':skip',
+      max: 'upper %d chars',
+      min: '%d chars at least',
+      emptyWarning: 'can not be empty',
+      upperLimitWarning: 'over limit',
+      lowerLimitWarning: 'below limit',
+    },
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing:",
+        enum: {
+          feat: {
+            description: 'A new feature',
+            title: 'Features',
+            emoji: '✨',
+          },
+          fix: {
+            description: 'A bug fix',
+            title: 'Bug Fixes',
+            emoji: '🐛',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📚',
+          },
+          style: {
+            description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+            title: 'Styles',
+            emoji: '💎',
+          },
+          refactor: {
+            description: 'A code change that neither fixes a bug nor adds a feature',
+            title: 'Code Refactoring',
+            emoji: '📦',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            title: 'Performance Improvements',
+            emoji: '🚀',
+          },
+          test: {
+            description: 'Adding missing tests or correcting existing tests',
+            title: 'Tests',
+            emoji: '🚨',
+          },
+          build: {
+            description: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+            title: 'Builds',
+            emoji: '🛠',
+          },
+          ci: {
+            description: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+            title: 'Continuous Integrations',
+            emoji: '⚙️',
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: 'Chores',
+            emoji: '♻️',
+          },
+          revert: {
+            description: 'Reverts a previous commit',
+            title: 'Reverts',
+            emoji: '🗑',
+          },
+        },
+      },
+      scope: {
+        description:
+          'What is the scope of this change (e.g. component or file name)',
+      },
+      subject: {
+        description: 'Write a short, imperative tense description of the change',
+      },
+      body: {
+        description: 'Provide a longer description of the change',
+      },
+      isBreaking: {
+        description: 'Are there any breaking changes?',
+      },
+      breakingBody: {
+        description:
+          'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
+      },
+      breaking: {
+        description: 'Describe the breaking changes',
+      },
+      isIssueAffected: {
+        description: 'Does this change affect any open issues?',
+      },
+      issuesBody: {
+        description:
+          'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
+      },
+      issues: {
+        description: 'Add issue references (e.g. "fix #123", "re #123".)',
+      },
+    },
   },
 };
