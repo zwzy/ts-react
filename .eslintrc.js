f3ffd55bb644f81ad8065ede10f1f8f804d83f1e
zuowang <18755489161@163.com>
2021-08-05 15:31:45 +0800

    chore(eslintrc): 忽略eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,26 @@ module.exports = {
     'react',
     '@typescript-eslint',
   ],
+  // 解决引入.ts 模块报错
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+          '.jsx',
+        ],
+      },
+    },
+  },
   rules: {
+    // 关闭props?时 defaultProps的强制指定
+    'react/require-default-props': [0],
     'no-use-before-define': 'off', // 解决 typescript - eslint import React from 'react' 报错
     '@typescript-eslint/no-use-before-define': ['error'], // 解决 typescript - eslint import React from 'react' 报错
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }], // jsx 文件名结尾支持 .tsx
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.jsx'] }], // jsx 文件名结尾支持 .tsx
+    // 解决引入.ts 模块报错
+    'import/extensions': ['error', 'never'], // jsx 文件名结尾支持 .tsx
   },
 };
